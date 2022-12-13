26597803de9efdb67e935ba914e81db9f2b40982
zhakypov50 <115090188+zhakypov50@users.noreply.github.com>
2022-12-14 00:46:40 +0600

    DZ2_Nurtilek_19-01react1

---
+++ src/MyInfo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class MyInfo extends React.Component {
+    render()
+    {
+        return (
+            <div>
+                <h1>Nurtilek</h1>
+                <p>I'm a beginner Frontend developer</p>
+                <ul>
+                    <li>Los Angeles</li>
+                    <li>New York</li>
+                    <li>San Francisco</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default MyInfo;
